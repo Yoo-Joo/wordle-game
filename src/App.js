8226032a9e815c9524361b200ef017e02c33b4d4
Youssef-GitHub <ermilijosef@gmail.com>
2022-06-02 22:40:10 +0100

    Json server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Wordle (Lingo)</h1>
-    </div>
-  );
+    const [solution, setSolution] = useState(null)
+
+    useEffect(() => {
+        fetch('http://localhost:3001/solutions')
+            .then(res => res.json())
+            .then(json => {
+                const randomSolution = json[Math.floor(Math.random() * json.length)]
+                setSolution(randomSolution.word)
+            })
+    }, [setSolution])
+
+    return (
+        <div className="App">
+            <h1>Wordle (Lingo)</h1>
+            {solution && <div>Solution is : {solution}</div>}
+        </div>
+    );
 }
 
 export default App
