964ec257ca8e6fafb5a8800ed6f0fd70629f63aa
Youssef-GitHub <ermilijosef@gmail.com>
2022-06-02 23:37:11 +0100

    Submitting guesses

--- src/hooks/useWordle.jsx
+++ src/hooks/useWordle.jsx
@@ -4,11 +4,11 @@ function useWordle(solution) {
     const [turn, setTurn] = useState(0)
     const [currentGuess, setCurrentGuess] = useState('')
     const [guesses, setGuesses] = useState([]) // each guess is an array
-    const [history, setHistory] = useState([]) // each guess is a string
+    const [history, setHistory] = useState(['ninja']) // each guess is a string
     const [isCorrect, setIsCorrect] = useState(false)
 
     const formatGuess = () => {
-
+        console.log('formatting the guess -', currentGuess)
     }
 
     const addNewGuess = () => {
@@ -22,6 +22,22 @@ function useWordle(solution) {
                 return prev.slice(0, -1)
             })
             return
+        }
+
+        if (key === 'Enter') {
+            if (turn > 5) {
+                console.log('u used all ur guesses')
+                return
+            }
+            if (history.includes(currentGuess)) {
+                console.log('you already tried that word')
+                return
+            }
+            if (currentGuess.length !== 5) {
+                console.log('word must be 5 chars long')
+                return
+            }
+            formatGuess()
         }
 
         if (/^[A-Za-z]$/.test(key)) {
