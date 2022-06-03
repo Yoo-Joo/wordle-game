d9af875ea2b23d6c6f83936f1d3dfb95247a0511
Youssef-GitHub <ermilijosef@gmail.com>
2022-06-03 15:57:04 +0100

    Showing past guesses

--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -5,7 +5,7 @@ function Grid({ currentGuess, guesses, turn }) {
     return (
         <div>
             {guesses.map((g, i) => {
-                return <Row key={i} />
+                return <Row key={i} guess={g} />
             })}
         </div>
     );
--- src/components/row.jsx
+++ src/components/row.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
-function Row() {
-    
+function Row({ guess }) {
+
+    if (guess) {
+        return (
+            <div className='row past'>
+                {guess.map((l ,i) => (
+                    <div key={i} className={l.color}>{l.key}</div>
+                ))}
+            </div>
+        )
+    }
+
     return (
         <div className='row'>
             <div></div>
