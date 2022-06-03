09e6a2d70a20cefbc0d4bf04c2abdeb3ffc3a60d
Youssef-GitHub <ermilijosef@gmail.com>
2022-06-03 16:09:44 +0100

    Showing the current guess

--- src/components/row.jsx
+++ src/components/row.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 
-function Row({ guess }) {
+function Row({ guess, currentGuess }) {
 
     if (guess) {
         return (
             <div className='row past'>
                 {guess.map((l ,i) => (
                     <div key={i} className={l.color}>{l.key}</div>
+                ))}
+            </div>
+        )
+    }
+
+    if (currentGuess) {
+
+        let letters = currentGuess.split('')
+
+        return (
+            <div className='row current'>
+                {letters.map((l, i) => (
+                    <div key={i} className='filled'>{l}</div>
+                ))}
+                {[...Array(5 - letters.length)].map((_, i) => (
+                    <div key={i}></div>
                 ))}
             </div>
         )
